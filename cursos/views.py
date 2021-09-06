37f8024dcb6fbb8abba6770e7b0d7c53d47ae6c5
diegobachsi <diegobachsi@gmail.com>
2021-09-06 18:28:34 -0300

    Atualizando templates cursos

--- cursos/views.py
+++ cursos/views.py
@@ -7,10 +7,11 @@ from .models import Cursos
 
 @login_required(login_url='accounts:login')
 def cursos(request):
+
     cursos = Cursos.objects.all()
     template_name = 'cursos.html'
     context = {
-        'cursos': cursos
+        'cursos': cursos,
     }
     return render(request, template_name, context)
 
@@ -28,12 +29,21 @@ def details(request, slug):
 
 @login_required(login_url='accounts:login')
 def videos_por_cursos(request, id):
+
+    duration = 0
+
     curso = Cursos.objects.filter(id=id)
-    print(curso)
     videos = Videos.objects.filter(curso=id)
+    segundos = Videos.objects.filter(curso=id).values('segundos')
+
+    for i in range(videos.count()):
+        duration += segundos[i]['segundos']
+    
     template_name = 'videos_por_cursos.html'
     context = {
         'videos': videos,
-        'cursos': curso
+        'cursos': curso,
+        'duration': duration / 60,
+        'qtd_videos': videos.count()
     }
     return render(request, template_name, context)
